68a662f01abe81a3eaf96daf9167ac75b6d6389f
Jinnawat Lochaiyakul <Jinnawat Lochaiyakul>
2016-03-27 15:14:24 +0700

    Fix describe

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect
 
-describe('#indexOf()', function() {
-	it('1+1 === 2', function() {
+describe('Chai Test addition 1 By 1 and expect Equal 2', function() {
+	it('1+1 = 2', function() {
 	  expect(1+1).to.equal(2);
 	});
 });
